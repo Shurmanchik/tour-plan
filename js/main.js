d0deed17da3f21d865ce25fa8f94d5e579f948b6
academy.glo <shurmin.2013@gmail.com>
2020-12-21 20:52:51 +0300

    create: added map in hotel section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,/*перелистывает в начало когда дойдет до последней картинки */
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 
   // Navigation arrows
   navigation: {
