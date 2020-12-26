d7f0627c02f856ef78424eceae7b134c062f663b
academy.glo <shurmin.2013@gmail.com>
2020-12-26 18:50:06 +0300

    crate: added jquery and modal overlay

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,/*перелистывает в начало когда дойдет до последней картинки */
   keyboard: {
@@ -29,10 +30,28 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   },
 });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function() {
-  console.log("клик")
-  document
-    .querySelector(".navbar-button")
-    .classList.toggle("navbar-button--visible")
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function() {
+    console.log("клик")
+    $(".navbar-button")
+      .toggleClass("navbar-button--visible")
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
